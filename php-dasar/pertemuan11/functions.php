1eefa2b6c6b77418a59b37686df8591164326493
mmhdagungg <mmhdagung7@gmail.com>
2026-03-31 18:59:30 +0700

    menambahkan fitur tambah

---
+++ php-dasar/pertemuan11/functions.php
@@ -0,0 +1,57 @@
+<?php
+// koneksi database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data)
+{
+    global $conn;
+
+    $npm = htmlspecialchars($data["npm"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+    $email = htmlspecialchars($data["email"]);
+
+    $query = "INSERT INTO mahasiswa VALUES ('', '$nama', '$npm', '$email', '$jurusan', '$gambar')";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+    global $conn;
+
+    $id = htmlspecialchars($data["id"]);
+    $npm = htmlspecialchars($data["npm"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $query = "UPDATE mahasiswa SET
+    npm = '$npm',
+    nama = '$nama',
+    email = '$email',
+    jurusan = '$jurusan',
+    gambar = '$gambar'
+    WHERE id = '$id'";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+
+?>
---
+++ php-dasar/pertemuan11/index.php
@@ -0,0 +1,64 @@
+<?php
+
+require 'functions.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+    <br>
+
+
+    <table border="1" cellpading="15" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NPM</th>
+            <th>Jurusan</th>
+            <th>Nama</th>
+            <th>Email</th>
+        </tr>
+        <?php $i = 1; ?>
+        <?php foreach ($mahasiswa as $row): ?>
+            <tr>
+                <td>
+                    <?= $i; ?>
+                </td>
+                <td>
+                    <a href="ubah.php?id=<?= $row['id'] ?>">Ubah</a> |
+                    <a href="hapus.php?id=<?= $row["id"] ?>" onclick="return confirm('yakin ingin menghapus?')">Hapus</a>
+                </td>
+                <td>
+                    <img src="img/<?php echo $row["gambar"] ?>" alt="" width=110px>
+                </td>
+                <td>
+                    <?= $row["npm"]; ?>
+                </td>
+                <td>
+                    <?= $row["jurusan"]; ?>
+                </td>
+                <td>
+                    <?= $row["nama"]; ?>
+                </td>
+                <td>
+                    <?= $row["email"]; ?>
+                </td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach ?>
+    </table>
+</body>
+
+</html>
